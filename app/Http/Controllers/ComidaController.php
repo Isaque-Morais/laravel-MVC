b0a082b034966589d0022db424e4f4915f82883a
Isaque-Morais <isaquemorais5@gmail.com>
2023-09-13 16:38:29 -0300

    fix: Ajustado parametro de função

--- app/Http/Controllers/ComidaController.php
+++ app/Http/Controllers/ComidaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ComidaController extends Controller
 {
-    public function index(Resquest $request)
+    public function index(Request $request)
     {
 
         $comidaPraSerie = [
