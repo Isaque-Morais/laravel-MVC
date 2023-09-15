7d259e4ccd2eb19b1876f6b102dfd5ba58466a70
Isaque-Morais <isaquemorais5@gmail.com>
2023-09-15 14:57:33 -0300

    feat: Criado o layout padrão usando blade

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }} Conteúdo de  séries</title>
+    <title>{{ $title }} - Conteúdo de  séries</title>
+    <link rel="stylesheet" href="{{ asset('css/app.scss') }}">
 </head>
 <body>
 <h1>{{ $title }}</h1>
